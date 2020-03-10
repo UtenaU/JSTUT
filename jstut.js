622e406ae570c51f32bf85138763d6bccdf23354
nadia <wzmjohn2018@163.com>
2020-03-10 23:05:19 +0800

    not made by myself

--- jstut.js
+++ jstut.js
@@ -33,46 +33,69 @@ function fib(whichNum){
     return num2;
 }
 
-let mLText = "My dear old ~ sat me down to hear some words of wisdom \n 1. Give a man a ~ and you ~ him for a day ~ a man to ~ and he'll ~ forever \n 2. He who ~ at the right time can ~ again \n 3. Always wear ~ ~ in case you're in a ~ \n 4. Don't use your ~ to wipe your ~ Always have a clean ~ with you";
-//Convert the string into array where spaces determine
-let mLArray = mLText.split(" ");
-//Get a input array
-let inputArray = [];
-
-function madLibGenerator(){
+  // ----- MADLIB GENERATOR -----
+ 
+  // Create the MadLib using a ~ every place we want to 
+  // enter a user supplied string
+  let mLText = "My dear old ~ sat me down to hear some words of wisdom \n 1. Give a man a ~ and you ~ him for a day ~ a man to ~ and he'll ~ forever \n 2. He who ~ at the right time can ~ again \n 3. Always wear ~ ~ in case you're in a ~ \n 4. Don't use your ~ to wipe your ~ Always have a clean ~ with you";
+ 
+  // Convert the string into an array where spaces determine
+  // the individual pieces to put in each array cell
+  let mLArray = mLText.split(" ");
+ 
+  // Array that will hold user input
+  let inputArray = [];
+ 
+  function madLibGenerator(){
     createInputArray();
+ 
+    // If this function returns true we know the user
+    // didn't supply new data
     if(checkForMissingInput()){
         document.getElementById("output1").value = "Enter all values above";
-    }else{
+ 
+    // If they did supply new data we can create the
+    // custom MadLib
+    } else {
         createMLSentence();
     }
-}
-
-function createInputArray(){
+  }
+ 
+  function createInputArray(){
+    
+    // Get all the values the user entered by incrementing
+    // from 0 to 13 to match the given ids 
     for(i = 0; i <= 13; i++){
         inputArray[i] = document.getElementById("i" + i).value;
     }
-}
-
-function checkForMissingInput(){
+  }
+ 
+  function checkForMissingInput(){
+    
+    // Create an array with the default values
     let defaultArrayVals = ["Person", "Noun", "Verb", "Adjective", "Plural Verb", "Body Part", "Event"];
-
-    for(i = 0;i < inputArray.length;i++){
+ 
+    // length tells us how many values the array has
+    for(i=0; i < inputArray.length; i++){
+        // Checks to see if input matches any of the defaults
+        // If there are no matches a -1 is returned
         if(defaultArrayVals.indexOf(inputArray[i]) > -1 ){
             return true;
         }
     }
     return false;
-}
-// Cycle through all words in our default MadLib and
-// every place there is a ~ put in the user supplied 
-// values
-function createMLSentence(){
+  }
+ 
+  // Cycle through all words in our default MadLib and
+  // every place there is a ~ put in the user supplied 
+  // values
+  function createMLSentence(){
     let arrIndex = 0;
-    for(i = 0;i < mLArray.length;i++){
+    for(i=0; i < mLArray.length; i++){
         let matchIndex = mLArray.indexOf("~");
         mLArray[matchIndex] = inputArray[arrIndex];
         arrIndex++;
     }
+ 
     document.getElementById("output1").value = mLArray.join(" ");
-}
+  }
